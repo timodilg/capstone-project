46ba4cbb3fd43e8f1c7c4668c0baa475ff546497
Timo Dilg <timo_dilg@yahoo.de>
2020-03-09 14:14:35 +0100

    Initial commit timer

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -1,5 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components/macro'
+import moment from 'moment'
 
-export default function Timer() {
-  return <div>Timer</div>
+class Timer extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentTime: moment.duration(25, 'minutes'),
+      baseTime: moment.duration(25, 'minutes'),
+    }
+  }
+
+  startTimer() {
+    this.setState({
+      timerState: timerStates.RUNNING,
+    })
+  }
+
+  render() {
+    return (
+      <TimerStyled>
+        <h2>25:00</h2>
+        <div>
+          <button>Start</button>
+          <button>Pause</button>
+          <button>Stop</button>
+        </div>
+      </TimerStyled>
+    )
+  }
 }
+
+const TimerStyled = styled.div`
+  margin: 200px auto;
+  font-size: 20px;
+  text-align: center;
+
+  /* div {
+    display: flex;
+    justify-content: space-between;
+  } */
+
+  button {
+    transform: scale(1.5);
+  }
+`
